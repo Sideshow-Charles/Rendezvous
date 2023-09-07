8b1fe08612c086b3a4825bc87a9e49f1fdcaf961
Charles Onuoha <charlesonuoha97@gmail.com>
2023-09-07 16:20:58 +0100

    cleaned up the nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: "plugins/axios.js", ssr: true },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -43,7 +42,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // '@nuxtjs/axios',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
