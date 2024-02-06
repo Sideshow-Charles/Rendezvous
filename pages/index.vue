51dc9a9d7b4cf72c88e7689ce7bf918e305cbdce
charles144 <charles@flutterwavego.com>
2024-02-06 17:26:27 +0100

    fixed issue with loader

--- pages/index.vue
+++ pages/index.vue
@@ -32,9 +32,16 @@
         <!-- Event Cards -->
         <!-- Loader -->
         <div v-if="loading" class="loader">
-          <img src="../assets/svgs/loader.svg" alt="">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            style="margin: auto; background: rgb(255, 255, 255); display: block; shape-rendering: auto;" width="200px"
+            height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <circle cx="50" cy="50" fill="none" stroke="#783ead" stroke-width="10" r="35"
+              stroke-dasharray="164.93361431346415 56.97787143782138">
+              <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s"
+                values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+            </circle>
+          </svg>
         </div>
-
         <!-- End of Loader -->
 
         <div v-else class="event__card__container">
@@ -185,6 +192,13 @@ export default {
   justify-content: space-between;
   align-items: center;
   margin: 0 64px;
+}
+
+.loader {
+  margin-top: 50px;
+  margin-bottom: 0;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .error {
@@ -462,5 +476,4 @@ export default {
   .discover__text {
     width: 100%;
   }
-}
-</style>
+}</style>
