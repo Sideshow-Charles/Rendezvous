50c8aa693699f085dc1da7f3f567f2cffe4dfc2a
charles144 <charles@flutterwavego.com>
2024-02-06 17:29:04 +0100

    fixed issue with loader

--- pages/index.vue
+++ pages/index.vue
@@ -33,14 +33,15 @@
         <!-- Loader -->
         <div v-if="loading" class="loader">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-            style="margin: auto; background: rgb(255, 255, 255); display: block; shape-rendering: auto;" width="200px"
-            height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            style="margin: auto; background: rgb(255, 255, 255); display: block; shape-rendering: auto;" width="80px"
+            height="80px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
             <circle cx="50" cy="50" fill="none" stroke="#783ead" stroke-width="10" r="35"
               stroke-dasharray="164.93361431346415 56.97787143782138">
               <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s"
                 values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
             </circle>
-          </svg>
+          </svg> <br>
+          Loading Events...
         </div>
         <!-- End of Loader -->
 
